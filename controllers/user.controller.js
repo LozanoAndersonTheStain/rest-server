f397a141a53bcc5cb3ac789dcca7ad235241c84b
LozanoTheStain <adslozanov@gmail.com>
2022-10-14 20:20:14 -0500

    Rest-server-básico

---
+++ controllers/user.controller.js
@@ -0,0 +1,60 @@
+const { request, response } = require('express')
+
+const getUsers = (req = request, res = response) => {
+  // url/api/users/?name=Sergio&date=2022-10-31 -> query
+
+  const { name, date } = req.query
+
+  req.res.status(200).json({
+    msg: 'Get - Controller',
+    name,
+    date,
+  })
+}
+
+const getUsersById = (req = request, res = response) => {
+  // url/api/users/14 -> Segmento: El 14 entra en el id
+
+  const id = req.params.id
+  res.json({
+    msg: 'Usuario por id - Controler',
+    id,
+  })
+}
+
+const createUser = (req, res) => {
+  // url/api/users/-> Body: Es el objeto en JSON
+
+  const { name, lastname, age } = req.body
+
+  res.status(201).json({
+    msg: 'post API - Controller',
+    name,
+  })
+}
+
+const updateUser = (req = request, res = response) => {
+  const id = req.params.id
+  const body = req.body
+
+  res.json({
+    msg: 'put API - Controller',
+    id,
+    body,
+  })
+}
+
+const deleteUser = (req = request, res = response) => {
+  const id = req.params.id
+  res.json({
+    msg: 'delete API - Controller',
+  })
+}
+
+module.exports = {
+  getUsers,
+  getUsersById,
+  createUser,
+  updateUser,
+  deleteUser,
+}
---
+++ models/server.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const cors = require('cors')
+
+class Server {
+  constructor() {
+    this.app = express()
+    this.port = process.env.PORT
+    this.usersPath = '/api/users'
+
+    // Middlewares
+    this.middlewares()
+
+    //Lectura y parseo del body
+    this.app.use(express.json())
+
+    // Rutas de mi app
+    this.routes()
+  }
+
+  middlewares() {
+    // CORS
+    this.app.use(cors())
+
+    // Directorio público
+    this.app.use(express.static('public'))
+  }
+
+  routes() {
+    this.app.use(this.usersPath, require('../routes/user.routes'))
+  }
+
+  listen() {
+    this.app.listen(this.port, () => {
+      console.log(`App escuchando en http://localhost:${this.port}`)
+    })
+  }
+}
+
+module.exports = Server
